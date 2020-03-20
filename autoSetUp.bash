641fc4d8068d421d73c6d6696cd5fc9e94ad757a
AnJung <jimmmy0719191@gmail.com>
2020-03-20 21:08:00 +0800

    split file

--- autoSetUp.bash
+++ autoSetUp.bash
@@ -14,37 +14,8 @@ bye() {
 # variables
 extendPKG=""
 favoratePKG="nyancat zsh docker docker-compose npm htop nmap git wget curl vim"
-useYum=false
 
 # main
-
-## change root password
-echo "wanna change root password? y/n"
-read isChRootpwd
-isChRootpwd=`echo $isChRootpwd | tr '[A-Z]' '[a-z]'`
-if [ $isChRootpwd == 'y' ] || [ $isChRootpwd == 'yes' ];
-then
-    sudo passwd root
-fi
-
-## change user
-echo "wanna add a new user and login as the new user? y/n"
-read isAddUser
-isAddUser=`echo $isAddUser | tr '[A-Z]' '[a-z]'`
-if [ $isAddUser == 'y' ] || [ $isAddUser == 'yes' ];
-then
-    echo "new userName:"
-    read newUserName
-    
-    sudo useradd -m $newUserName
-    sudo passwd $newUserName
-
-    echo "new user id:"
-    id $newUserName
-
-    echo "switch to $newUserName"
-    su $newUserName
-fi
 
 ## install pkg
 whichApt=`which apt`
---
+++ setUpUsers.bash
@@ -0,0 +1,41 @@
+#! bash
+# help functions
+help() {
+    echo "usage: 
+        -a \"pkg name\": add some other pkg you want"
+}
+
+bye() {
+    echo "bye. OwO"
+    exit 1
+}
+
+# main
+
+## change root password
+echo "wanna change root password? y/n"
+read isChRootpwd
+isChRootpwd=`echo $isChRootpwd | tr '[A-Z]' '[a-z]'`
+if [ $isChRootpwd == 'y' ] || [ $isChRootpwd == 'yes' ];
+then
+    sudo passwd root
+fi
+
+## change user
+echo "wanna add a new user and login as the new user? y/n"
+read isAddUser
+isAddUser=`echo $isAddUser | tr '[A-Z]' '[a-z]'`
+if [ $isAddUser == 'y' ] || [ $isAddUser == 'yes' ];
+then
+    echo "new userName:"
+    read newUserName
+    
+    sudo useradd -m $newUserName
+    sudo passwd $newUserName
+
+    echo "new user id:"
+    id $newUserName
+
+    echo "switch to $newUserName"
+    su $newUserName
+fi
